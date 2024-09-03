bc9859c9e9b343412cd1d9d6a64de10605a30e7e
AndreiRupertti <arupertti@gmail.com>
2024-09-03 05:30:30 -0300

    set cron for gamespot for 3 hours

--- src/workers/config.ts
+++ src/workers/config.ts
@@ -1,6 +1,4 @@
 import { Bots } from "@/common/enums/bots";
 import { messagePostHandler } from "./message-post/message-post-handler";
 
-export default [
-    { name: Bots.GAMESPOT, cronTimeframe: '*/10 * * * * *', handler: messagePostHandler(Bots.GAMESPOT) }
-]
+export default [{ name: Bots.GAMESPOT, cronTimeframe: "0 */3 * * *", handler: messagePostHandler(Bots.GAMESPOT) }];
